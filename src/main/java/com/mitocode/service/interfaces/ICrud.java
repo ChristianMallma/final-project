ed4a8cb3a567ca4bc95af67ecfa9df51f55ebe63
Christian <christianmichelmallma@gmail.com>
2025-07-23 05:08:41 -0500

    Adding ICrud Interface

---
+++ src/main/java/com/mitocode/service/interfaces/ICrud.java
@@ -0,0 +1,14 @@
+package com.mitocode.service.interfaces;
+
+import com.mitocode.model.Student;
+
+import java.util.List;
+
+public interface ICrud<T, ID> {
+
+    T save(T t) throws Exception;
+    T update(ID id, T t) throws Exception;
+    List<T> findAll() throws Exception;
+    T findById(ID id) throws Exception;
+    void delete(ID id) throws Exception;
+}
--- src/main/java/com/mitocode/service/interfaces/IStudentService.java
+++ src/main/java/com/mitocode/service/interfaces/IStudentService.java
@@ -2,13 +2,6 @@ package com.mitocode.service.interfaces;
 
 import com.mitocode.model.Student;
 
-import java.util.List;
+public interface IStudentService extends ICrud<Student, Integer> {
 
-public interface IStudentService {
-
-    Student save(Student student) throws Exception;
-    Student update(Integer id, Student student) throws Exception;
-    List<Student> findAll() throws Exception;
-    Student findById(Integer id) throws Exception;
-    void delete(Integer id) throws Exception;
 }
