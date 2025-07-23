adb5f5130c1d0e189543d4d882f36b8b25595040
Christian <christianmichelmallma@gmail.com>
2025-07-23 05:02:58 -0500

    Adding IGenericRepo Interface

--- src/main/java/com/mitocode/repository/interfaces/IStudentRepo.java
+++ src/main/java/com/mitocode/repository/interfaces/IStudentRepo.java
@@ -1,7 +1,6 @@
 package com.mitocode.repository.interfaces;
 
 import com.mitocode.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IStudentRepo extends JpaRepository<Student, Integer> {
+public interface IStudentRepo extends IGenericRepo<Student, Integer> {
 }
