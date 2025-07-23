75306ad4d80def37eb123fc3f60f15efa10efcd0
Christian <christianmichelmallma@gmail.com>
2025-07-23 06:45:25 -0500

    Adding MapperConfig class and auxiliary functions for mapper (toDto, toEntity)

--- src/main/java/com/mitocode/controller/StudentController.java
+++ src/main/java/com/mitocode/controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.mitocode.controller;
 
+import com.mitocode.domain.dto.StudentDto;
 import com.mitocode.domain.model.Student;
 import com.mitocode.service.interfaces.IStudentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +17,46 @@ import java.util.List;
 public class StudentController {
 
     private final IStudentService studentService;
+    private final ModelMapper modelMapper;
 
     @GetMapping
-    public ResponseEntity<List<Student>> findAll() throws Exception {
-        List<Student> list = studentService.findAll();
+    public ResponseEntity<List<StudentDto>> findAll() throws Exception {
+        List<StudentDto> list = studentService.findAll().stream()
+                .map(this::mapToDto)
+                .toList();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Student> findById(@PathVariable("id") Integer id) throws Exception {
-        Student student = studentService.findById(id);
+    public ResponseEntity<StudentDto> findById(@PathVariable("id") Integer id) throws Exception {
+        StudentDto student = mapToDto(studentService.findById(id));
         return ResponseEntity.ok().body(student);
     }
 
     @PostMapping
-    public ResponseEntity<Student> save(@RequestBody Student student) throws Exception {
-        Student newStudent = studentService.save(student);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newStudent);
+    public ResponseEntity<StudentDto> save(@RequestBody StudentDto studentDto) throws Exception {
+        Student newStudent = studentService.save(mapToEntity(studentDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapToDto(newStudent));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Student> save(@PathVariable("id") Integer id, @RequestBody Student student) throws Exception {
-        Student updatedStudent = studentService.update(id, student);
-        return ResponseEntity.ok().body(updatedStudent);
+    public ResponseEntity<StudentDto> save(@PathVariable("id") Integer id, @RequestBody StudentDto studentDto) throws Exception {
+        Student updatedStudent = studentService.update(id, mapToEntity(studentDto));
+        return ResponseEntity.ok().body(mapToDto(updatedStudent));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) throws Exception {
         studentService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Auxiliary mapping functions
+    private StudentDto mapToDto(Student student) {
+        return modelMapper.map(student, StudentDto.class);
+    }
+
+    private Student mapToEntity(StudentDto studentDto) {
+        return modelMapper.map(studentDto, Student.class);
     }
 }
